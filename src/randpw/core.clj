726df7c7989bf16ea7ae6f7a59b65ac6069ec4d5
Darren Fix <darren.fix@gmail.com>
2017-09-15 17:04:32 -0600

    it's frickin workinggit add .git add .!

--- src/randpw/core.clj
+++ src/randpw/core.clj
@@ -10,21 +10,25 @@
 
 (defn get-word
   "the EFF dice list goes from 11111 to 66666"
-  []
-  (+ 11111 (rand-int 55555)))
+  [die]
+  (str/join (take die (repeatedly  #(+ 1 (rand-int 6))))))
 
 (def wordlists {:long   {:filename "resources/eff_large_wordlist.txt"
-                         :sha256 "addd35536511597a02fa0a9ff1e5284677b8883b83e986e43f15a3db996b903e"}
+                         :sha256 "addd35536511597a02fa0a9ff1e5284677b8883b83e986e43f15a3db996b903e"
+                         :dice 5}
                 :short1 {:filename "resources/eff_short_wordlist_1.txt"
-                         :sha256 "8f5ca830b8bffb6fe39c9736c024a00a6a6411adb3f83a9be8bfeeb6e067ae69"}
+                         :sha256 "8f5ca830b8bffb6fe39c9736c024a00a6a6411adb3f83a9be8bfeeb6e067ae69"
+                         :dice 4}
                 :short2 {:filename "resources/eff_short_wordlist_2_0.txt"
-                         :sha256 "22b45c52e0bd0bbf03aa522240b111eb4c7c0c1d86c4e518e1be2a7eb2a625e4"}})
+                         :sha256 "22b45c52e0bd0bbf03aa522240b111eb4c7c0c1d86c4e518e1be2a7eb2a625e4"
+                         :dice 4}})
 
 (defn check-wordlist
   "make sure that the wordlist that we're using hasn't been modified"
   [wordlist]
-  (= (digest/sha-256 (io/as-file (wordlist :filename)))
-   (wordlist :sha256)))
+  (let [wl (wordlists wordlist)]
+  (= (digest/sha-256 (io/as-file (wl :filename)))
+   (wl :sha256))))
 
 
 (defn read-lines [filename]
@@ -39,35 +43,14 @@
 
 
 
-(def mavals (apply hash-map (flatten (map make-entry (read-lines ((wordlists :short1) :filename))))))
+(def mavals (apply hash-map (flatten (map make-entry (read-lines ((wordlists :long) :filename))))))
 
-; (:6665 mavals)
-(:6666 mavals)
-
-
-(hash-map (map make-entry (read-lines ((wordlists :short1) :filename))))
-
-
-(rest mavals)
-
-
-(hash-map "dude" "roy" "killer" "bob")
-(str/split "1111\tdude" #"\t" )
-
-(hash-map (keyword (first ["1111" "dude"])) (second ["1111" "dude"]))
-
-
-(read-lines ((wordlists :short1) :filename))
-
-((wordlists :long) :filename)
-
-(get-number)
-
-(get-word)
-
-
-;; return 5 or 10 passwords, with numbers... have the user choose one of them?
-
+(defn generate-words
+  [wordlist words-per-line]
+  (let [words (apply hash-map (flatten (map make-entry (read-lines ((wordlists wordlist) :filename)))))
+        dice ((wordlists wordlist) :dice)]
+    (if (check-wordlist wordlist)
+                        (str/join " " (map #(get words %) (take words-per-line (repeatedly #(keyword (str (get-word dice))))))))))
 
 
 
@@ -76,4 +59,7 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (str/join "\n" (repeatedly 20 #(generate-words :long 12 )))))
+
+
+
--- test/randpw/core_test.clj
+++ test/randpw/core_test.clj
@@ -5,12 +5,8 @@
 (deftest wordlist-checksum
   (check-wordlist (wordlists :long))
   (check-wordlist (wordlists :short1))
-  (check-wordlist (wordlists :short2))
-  (= false (check-wordlist (wordlists :short2))))
+  (check-wordlist (wordlists :short2)))
 
-(deftest wordlist-bad-checksum
-  (not (check-wordlist {:filename "resources/eff_large_wordlist.txt"
-                        :sha256 "this is an obviously bad checksum"})))
 
 
 
